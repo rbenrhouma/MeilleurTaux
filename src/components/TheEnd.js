69fc3ff3619cf04976bdb45bc6d03bd49a40d893
Ridha <rbenrhouma2@gmail.com>
2020-03-29 19:55:50 +0200

    retour

--- src/components/TheEnd.js
+++ src/components/TheEnd.js
@@ -16,33 +16,31 @@ const TheEnd = props => {
 
   const saveData = async () => {
     if (
-      // Champs obligatoires le code postal, email, type de bien ,  état ainsi que le montant total de l'emprunt.
-      devis.zipCode &&
-      devis.email &&
       devis.typeBien &&
-      devis.etatBien &&
-      devis.total !== undefined &&
-      devis.montant !== undefined
+      devis.usageBien &&
+      devis.email &&
+      devis.total !== undefined
     ) {
       try {
         const response = await axios.post(
           "https://meilleurtauxapi.herokuapp.com/devis/save",
           {
-            zipCode: devis.zipCode,
-            email: devis.email,
             typeBien: devis.typeBien,
-            etatBien: devis.etatBien,
-            total: devis.total,
-            montant: devis.montant,
-            usageBien: devis.usageBien ? devis.usageBien : "0",
-            situationUser: devis.situationUser ? devis.situationUser : "",
-            typeBienLib: devis.typeBienLib ? devis.typeBienLib : "",
-            etatBienLib: devis.etatBienLib ? devis.etatBienLib : "",
-            usageBienLib: devis.usageBienLib ? devis.usageBienLib : ":",
-            situationUserLib: devis.situationUserLib
-              ? devis.situationUserLib
-              : "",
-            notaire: devis.notaire ? devis.notaire : "0"
+            usageBien: devis.usageBien,
+            email: devis.email,
+            total: devis.total
+
+            // etatBien: devis.etatBien,
+            // situationUser: devis.situationUser,
+            // typeBienLib: devis.typeBienLib,
+            // etatBienLib: devis.etatBienLib,
+            // usageBienLib: devis.usageBienLib,
+            // situationUserLib: devis.situationUserLib,
+            // country: devis.country,
+            // zipCode: devis.zipCode,
+            // montant: devis.montant,
+            // travaux: devis.travaux,
+            // notaire: devis.notaire,
           }
         );
 
